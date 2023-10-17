e64debc68bbc49f2bb17abf9b02be095210ae680
infxnnite8 <dorianquintana@gmail.com>
2023-10-16 22:39:57 -0700

    activity-three

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-    res.send('GET /places')
-})
+function index (data) {
+  let placesFormatted = data.places.map((place) => {
+    return (
+      <div>
+        <h2>{place.name}</h2>
+        <img src={place.pic} alt={place.name}/>
+      </div>
+    )
+  })
+  return (
+    <Def>
+        <main>
+            <h1>PLACES INDEX PAGE</h1>
+            {placesFormatted}
+        </main>
+    </Def>
+)
+}
 
 module.exports = router
