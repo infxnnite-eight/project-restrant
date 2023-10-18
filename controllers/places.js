8916613c47d1689b8d59d9556be1e9120f313f58
infxnnite8 <dorianquintana@gmail.com>
2023-10-17 18:05:02 -0700

    Adding styles to REST-Rant

--- controllers/places.js
+++ controllers/places.js
@@ -1,22 +1,5 @@
 const router = require('express').Router()
 
-function index (data) {
-  let placesFormatted = data.places.map((place) => {
-    return (
-      <div>
-        <h2>{place.name}</h2>
-        <img src={place.pic} alt={place.name}/>
-      </div>
-    )
-  })
-  return (
-    <Def>
-        <main>
-            <h1>PLACES INDEX PAGE</h1>
-            {placesFormatted}
-        </main>
-    </Def>
-)
-}
+
 
 module.exports = router
--- views/default.jsx
+++ views/default.jsx
@@ -1,16 +1,21 @@
 const React = require('react')
-
 function Def (html) {
     return (
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+                <a href="/places"><button className="btn-primary">Places Page</button></a>
+                                                                       
+
             </head>
             <body>
                 {html.children}
             </body>
         </html>
     )
-  }
-  
+}
+
+
+
 module.exports = Def
